5060c022c610aa1cd899e5ffb7989fa5f1cfa799
vitorammm <117685977+vitorammm@users.noreply.github.com>
2025-05-22 19:19:54 +0200

    Update monitorPlotter.py
    update!@

--- monitorPlotter.py
+++ monitorPlotter.py
@@ -23,7 +23,7 @@ tensione_misurata = [float(val) for val in dati_grezzi]
 
 # === Simulazione segnali ideali ===
 tensione_simulata =  2.5 + 2.5 * np.sin(2 * np.pi * 3 * tempo / numero_campioni)
-corrente_simulata =  2.5 + 2.5 * np.sin(2 * np.pi * 3 * tempo / numero_campioni + np.pi/2)
+corrente_simulata =  2.5 * np.sin(2 * np.pi * 3 * tempo / numero_campioni + np.pi/2)
 
 # === Calcolo corrente reale dal segnale misurato ===
 from scipy.signal import savgol_filter
@@ -32,11 +32,11 @@ corrente_calcolata = capacita * np.gradient(tensione_filtrata)
 corrente_calcolata -= np.mean(corrente_calcolata)  # Rimuove offset
 
 # === Selezione porzione utile del segnale ===
-inizio = 50
+inizio = 0
 fine = 300
 tempo_tagliato = tempo[inizio:fine]
 tensione_sim_tagliata = tensione_simulata[inizio:fine]
-corrente_sim_tagliata = corrente_simulata[inizio:fine]
+corrente_sim_tagliata = corrente_simulata[inizio:fine] 
 tensione_mis_tagliata = np.array(tensione_misurata)[inizio:fine]
 corrente_calc_tagliata = corrente_calcolata[inizio:fine]
 
@@ -50,15 +50,15 @@ corrente_scalata = corrente_microA * fattore_scala
 # === Plot dei segnali ===
 plt.figure()
 plt.plot(tempo_tagliato, tensione_sim_tagliata, 'r-', label='Tensione Simulata')
-plt.plot(tempo_tagliato, corrente_sim_tagliata, 'b-', label='Corrente Simulata (sfasata)')
+#plt.plot(tempo_tagliato, corrente_sim_tagliata, 'b-', label='Corrente Simulata (sfasata)')
 plt.plot(tempo_tagliato, tensione_mis_tagliata, 'g--', label='Tensione Misurata da Arduino')
-plt.plot(tempo_tagliato, corrente_scalata, 'm--', label='Corrente Calcolata')
+#plt.plot(tempo_tagliato, corrente_scalata, 'm--', label='Corrente Calcolata')
 
-
-
+ 
+#Abbellimenti vari
 plt.xlabel('Tempo [campioni]')
 plt.ylabel('Ampiezza')
-plt.title('Segnali misurati e simulati (Vito Ramunno)')
+plt.title('Segnali misurati e simulati')
 plt.legend()
 plt.grid()
 plt.ylim(-5, 8)
